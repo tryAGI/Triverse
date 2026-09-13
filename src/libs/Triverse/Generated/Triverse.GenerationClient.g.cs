e735609f5fbf53c28088f9d78581fe33e833d4a8
Konstantin S. <havendv@gmail.com>
2026-09-13 19:47:22 +0400

    feat: Updated OpenAPI spec (#25)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Triverse/Generated/Triverse.GenerationClient.g.cs
+++ src/libs/Triverse/Generated/Triverse.GenerationClient.g.cs
@@ -34,7 +34,7 @@ namespace Triverse
         /// <inheritdoc/>
         public global::Triverse.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Triverse.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Triverse.GenerationSourceGenerationContext.Default);
 
         /// <summary>
         ///
